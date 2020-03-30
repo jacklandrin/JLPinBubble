46afe020980b056336c7db5217e9d2eadeec3c68
jack <jacklandrin@hotmail.com>
2020-03-30 22:04:12 +0800

    Initial Commit

---
+++ JLPinBubble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JLPinBubble
+//
+//  Created by jack on 2020/3/30.
+//  Copyright © 2020 jack. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
